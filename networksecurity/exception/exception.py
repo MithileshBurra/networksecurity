4b54a113f1626365ad81d02635cd97bc57fc87b5
Mithilesh Burra <mithileshburra@gmail.com>
2025-07-28 18:46:05 +0530

    Data Ingestion Commited

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,17 +1,14 @@
-import sys 
+import sys
 from networksecurity.logging import logger
 
-class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
+class NetworkSecurityException(Exception):
+    def __init__(self,error_message,error_details:sys):
         self.error_message = error_message
-        _,_,exc_tb = error_detail.exc_info()
-        self.lineno = exc_tb.tb_lineno
-        self.filename = exc_tb.tb_frame.f_code.co_filename 
+        _,_,exc_tb = error_details.exc_info()
+        
+        self.lineno=exc_tb.tb_lineno
+        self.file_name=exc_tb.tb_frame.f_code.co_filename 
+    
     def __str__(self):
-        retrun "Error occured at filename {0} line no{} Error Message {3}".format(
-            self.filename, self.lineno, str(self.error_message)
-        )
-
-
-
-     
+        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+        self.file_name, self.lineno, str(self.error_message))
